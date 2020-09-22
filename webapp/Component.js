e8c3a14dd15e787617feaa3153b4f869f5bc161d
ravivarmai <39600385+ravivarmai@users.noreply.github.com>
2020-09-22 15:39:47 -0700

    Add files via upload

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.autodesk.zfi_ob52_tb_log.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
